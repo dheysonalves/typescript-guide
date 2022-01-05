fc2ec0302d58cca83ccce7dc19121b56fcb16960
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-01-04 23:25:06 -0300

    feat: third lesson about primitive and object types

---
+++ lesson2/features/variables.ts
@@ -0,0 +1,16 @@
+// type annotation
+
+/** If you wanna consider that a variable has
+	exactly that type, you should consider type annotation */
+
+// Primitive types
+let apples: number = 5;
+let food: string = "ham";
+let hasName: boolean = true;
+let nothing: undefined | null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green"];
