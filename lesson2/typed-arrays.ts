dd145b4966d345be72766a4a591b7b8cf2ece422
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-01-05 00:46:51 -0300

    feat: adding typed-arrays example

---
+++ lesson2/typed-arrays.ts
@@ -0,0 +1,9 @@
+let carMakers: string[];
+const dates = [new Date(), new Date()];
+const carsByMake = [["f15"], ["corolla"]];
+
+// Flexible types
+const importantDates = [new Date(), "2030-10-10"];
+const importantValues: (Date | string)[] = [];
+
+importantValues.push("2031-10-01");
