82834b10db2fd93c67f37e96ccfcde5efbcbb116
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-01-09 12:48:26 -0300

    feat: update interface and add markerContent function with HTML

--- maps-project/src/Company.ts
+++ maps-project/src/Company.ts
@@ -1,19 +1,33 @@
 import faker from 'faker-br';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
 	name: string;
 	catchPhrase: string;
+	description: string;
 	location: {
 		lat: number;
 		log: number;
 	};
+	color = 'blue';
 
 	constructor() {
 		this.name = faker.company.companyName();
 		this.catchPhrase = faker.company.catchPhrase();
+		this.description = faker.company.catchPhraseDescriptor();
 		this.location = {
 			lat: parseFloat(faker.address.latitude()),
 			log: parseFloat(faker.address.longitude()),
 		};
+	}
+
+	markerContent(): string {
+		return `
+		<div>
+			<h3 style="color: ${this.color}; text-transform: uppercase;">Company name: ${this.name}</h3>
+			<h4>CatchPhrase: ${this.catchPhrase}</h4>
+			<p>${this.description}</p>
+		</div>
+		`;
 	}
 }
