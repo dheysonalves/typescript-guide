3d5876a67dcd9a23dcaf8859abbcc7d49ceaafeb
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-01-09 12:49:27 -0300

    feat: create an user and company object

--- maps-project/src/index.ts
+++ maps-project/src/index.ts
@@ -2,14 +2,14 @@
 
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomClass} from './CustomMap';
 
 const user = new User();
 const company = new Company();
 
-new google.maps.Map(document.getElementById("maps"), {
-	zoom: 1,
-	center: {
-		lat: user.location.lat,
-		lng: user.location.log,
-	},
-});
+const customMap = new CustomClass('maps');
+
+// TODO Add property to create multiple instances.
+customMap.addMarker(user);
+customMap.addMarker(company);
+
